4d7b33b77fe3d923eb88510cef62574490b687c6
jmaulinasari <jmaulinasari@gmail.com>
2026-05-29 14:45:17 +0700

    update

--- Angkatan2-2026/project_crud/pages/category.php
+++ Angkatan2-2026/project_crud/pages/category.php
@@ -1,41 +1,32 @@
 <?php
 include "config/koneksi.php";
-$query = mysqli_query($koneksi, "SELECT * FROM categories ORDER BY id DESC");
-
-
-
-$rows = mysqli_fetch_all($query, MYSQLI_ASSOC);
-
+$selectCategories = mysqli_query($koneksi, "SELECT * FROM categories ORDER BY id DESC");
+$rowCategories = mysqli_fetch_all($selectCategories, MYSQLI_ASSOC);
 
 if (isset($_GET['delete'])) {
-    $id = $_GET['delete'];
+    $id = $_GET['delete'] ?? 0;
+
     $delete = mysqli_query($koneksi, "DELETE FROM categories WHERE id='$id'");
-    header("location:?page=category");
-    exit();
+    if ($delete) {
+        header("location:?page=category");
+        exit();
+    }
 }
-
 ?>
-
 <div class="card">
-    <h5 class="card-header">
-        Menu
-    </h5>
+    <div class="card-header">
+        <h5 class="card-header">
+            Manage Category
+        </h5>
+    </div>
     <div class="card-body">
-        <div class="mb-2" align="right">
-            <a href="?page=create-category" class="btn btn-primary">Create New Category</a>
+        <div class="mb-2 d-flex justify-content-end">
+            <a href="?page=create-category" class="btn btn-primary">Create Category</a>
         </div>
         <div class="table-responsive">
             <?php
             if (isset($_GET['status']) && $_GET['status'] == 'success') {
-                $status = "Category succesfully created";
-                $location = "?page=category";
-                echo statusSuccess($status, $location);
-            } elseif (isset($_GET['status']) && $_GET['status'] == 'category-exists') {
-                $status = "Category name is already exists";
-                $location = "?page=category";
-                echo inputFailed($status, $location);
-            } elseif (isset($_GET['status']) && $_GET['status'] == 'edited') {
-                $status = "Category succesfully saved";
+                $status = "Data Berhasil ditambah!";
                 $location = "?page=category";
                 echo statusSuccess($status, $location);
             }
@@ -44,25 +35,21 @@ if (isset($_GET['delete'])) {
                 <thead>
                     <tr>
                         <th>No</th>
-                        <th>Name</th>
-                        <th>Status</th>
+                        <th>Category Name</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    foreach ($rows as $index => $row) {
-                        $status = $row['is_active'];
+                    foreach ($rowCategories as $index => $v) {
                     ?>
                         <tr>
-
                             <td><?= $index + 1 ?></td>
-                            <td><?= $row['category_name'] ?></td>
-                            <td><?= getStatus($row['is_active']) ?></td>
+                            <td><?= $v['category_name'] ?></td>
                             <td>
-                                <a href="?page=create-category&edit=<?= $row['id'] ?>" class="btn btn-success">Edit</a>
-                                <form action="?page=category&delete=<?= $row['id'] ?>" method="post" class="d-inline">
-                                    <button class="btn btn-danger" onclick="return confirm('ARE YOU SURE?')">Delete</button>
+                                <a href="?page=create-category&edit=<?= base64_encode($v['id']) ?>" class="btn btn-success">Edit</a>
+                                <form action="?page=category&delete=<?= $v['id'] ?>" method="post" class="d-inline">
+                                    <button class="btn btn-danger" onclick="return confirm('Yakin ingin hapus ?')">Delete</button>
                                 </form>
                             </td>
                         </tr>
--- Angkatan2-2026/project_crud/pages/create-category.php
+++ Angkatan2-2026/project_crud/pages/create-category.php
@@ -1,107 +1,61 @@
 <?php
 include "config/koneksi.php";
-$query = mysqli_query($koneksi, "SELECT categories.category_name as category_name, categories.* FROM categories LEFT JOIN categories as category ON category.id = categories.id ORDER BY categories.id DESC");
-
-$rows = mysqli_fetch_all($query, MYSQLI_ASSOC);
-
-if (isset($_POST['create'])) {
-    $category_name =  htmlspecialchars($_POST['category_name']);
-    $status = htmlspecialchars($_POST['is_active']);
-
-    $cek = mysqli_query($koneksi, "SELECT * FROM categories WHERE category_name='$category_name'");
+if (isset($_POST['simpan'])) {
+    $category_name = htmlspecialchars($_POST['category_name']);
+    //CEK CATEGORIES :
+    $cek = mysqli_query($koneksi, "SELECT category_name FROM categories WHERE category_name='$category_name'");
     if (mysqli_num_rows($cek) > 0) {
-        header('location:?page=create-category&status=category-exists');
+        header("location:?page=create-category&status=category-exist");
         exit();
     }
-    $update = mysqli_query($koneksi, "INSERT INTO categories (category_name, is_active) VALUES ('$category_name','$status')");
-
-    if ($update) {
-        header('location:?page=category&status=success');
-        exit();
-    }
-}
-
-$id = $_GET['edit'] ?? '';
-// $id = isset($_GET['idEdit']) ? $_GET['idEdit'] : '';
-$selectCategories = mysqli_query($koneksi, "SELECT * FROM categories WHERE id='$id'");
-$edit = mysqli_fetch_assoc($selectCategories);
-
-if (isset($_POST['edit'])) {
-    $category_name = $_POST['category_name'];
-    $is_active = $_POST['is_active'];
-
-    $queryCek = mysqli_query($koneksi, "SELECT * FROM categories 
-    WHERE category_name='$category_name' AND id != '$id'");
-
-    if (mysqli_num_rows($queryCek) > 0) {
-        header("location:?page=create-category&edit=$   id&status=category-exist");
-        exit();
-    }
-
-    $query = mysqli_query($koneksi,  "UPDATE categories SET 
-    category_name='$category_name',
-    is_active='$is_active'
-    WHERE id='$id'");
-
+    // END CEK CATEGORIES
+    $query = mysqli_query($koneksi, "INSERT INTO categories (category_name) VALUES ('$category_name')");
     if ($query) {
-        header('location:?page=category&status=edited');
+        header("location:?page=category&status=success");
+        exit();
     }
 }
-// if (isset($_POST['save'])) {
-//     $category_name =  htmlspecialchars($_POST['category_name']);
-//     $status = htmlspecialchars($_POST['is_active']);
+if (isset($_GET['edit'])) {
+    $id = base64_decode($_GET['edit']) ?? '';
+    $select = mysqli_query($koneksi, "SELECT * FROM categories WHERE id='$id'");
+    $rowEdit = mysqli_fetch_assoc($select);
+    if (isset($_POST['edit'])) {
+        $category_name = htmlspecialchars($_POST['category_name']);
+        // CEK CATEGORIES :
+        $cek = mysqli_query($koneksi, "SELECT category_name FROM categories WHERE category_name='$category_name'");
+        if (mysqli_num_rows($cek) > 0) {
+            header("location:?page=create-category&edit=" . $_GET['edit'] . "&status=category-exist");
+            exit();
+        }
+        // END CEK CATEGORIES
+        $update = mysqli_query($koneksi, "UPDATE categories SET category_name='$category_name' WHERE id='$id'");
+        if ($update) {
+            header("location:?page=category&status=success");
+            exit();
+        }
+    }
+}
 
-//     $cek = mysqli_query($koneksi, "SELECT * FROM categories WHERE category_name='$category_name'");
-//     if (mysqli_num_rows($cek) > 0) {
-//         header('location:?page=create-category&status=category-exists');
-//         exit();
-//     }
-//     $update = mysqli_query($koneksi, "UPDATE categories SET category_name='$category_name', is_active='$status' WHERE id='$id'");
-
-//     if ($update) {
-//         header('location:?page=category&status=success');
-//         exit();
-//     }
-// }
 
 ?>
-
 <div class="card">
-    <h5 class="card-header">
-        <?= isset($_GET['edit']) ? 'Edit' : 'Create New' ?> Category
-    </h5>
+    <div class="card-header">
+        <h5 class="card-title">Create Category</h5>
+    </div>
     <div class="card-body">
         <form action="" method="post">
-
-            <div class="row">
-                <div class="col-6">
-                    <label for="" class="form-label">Name *</label>
-                    <input type="text" name="category_name" class="form-control" value="<?= isset($_GET['edit']) ? $edit['category_name'] : '' ?>" placeholder="Name" required>
-                    <?php
-                    if (isset($_GET['status']) && $_GET['status'] == 'category-exists') {
-                        $status = "Category name is already exists";
-                        echo inputFailed($status);
-                    }
-                    ?>
-                </div>
-                <div class="col-6">
-                    <label for="" class="form-label">Status *</label>
-                    <?php $activeStatus = $edit['is_active'] ?? 0 ?>
-                    <select class="form-select mb-3" aria-label="Default select" name="is_active">
-                        <option>Status Select</option>
-                        <option value="1" <?= ($activeStatus == 1) ? 'selected' : '' ?>>Active</option>
-                        <option value="0" <?= ($activeStatus  == 0) ? 'selected' : '' ?>>Inactive</option>
-                    </select>
-                </div>
-            </div>
-            <div class=" row mt-3">
-
-            </div>
-            <div class="text-end mt-4">
-                <button type="submit" class="btn btn-primary" name="<?= isset($_GET['edit']) ? 'edit' : 'create' ?>"><?= isset($_GET['edit']) ? 'Save' : 'Create' ?></button>
-                <a href="?page=category" class="btn btn-secondary">Back</a>
-            </div>
-
+            <label for="" class="form-label">Category Name</label>
+            <input type="text" class="form-control" name="category_name" value="<?= isset($id) ? $rowEdit['category_name'] : '' ?>" required>
+            <?php
+            $status = '';
+            if (isset($_GET['status']) && $_GET['status'] == 'category-exist') {
+                $status = "Category Name Already Exist!";
+            }
+            echo inputFailed($status);
+            ?>
+            <br>
+            <button type="submit" name="<?= isset($id) ? 'edit' : 'simpan' ?>" class="btn btn-primary mt-2"><?= isset($id) ? 'Update' : 'Create' ?></button>
+            <a href="?page=category" class="btn btn-secondary mt-2">Cancel</a>
         </form>
     </div>
 </div>
